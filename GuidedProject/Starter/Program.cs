4c67700f2275887449290ee5373ba7b7fcb47860
Breno Augusto <breaugustocp@outlook.com>
2025-11-05 21:47:36 -0300

    exercise 4 is finished

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -8,39 +8,37 @@ int[] andrewScores = [92, 89, 81, 96, 90];
 int[] emmaScores = [90, 85, 87, 98, 68];
 int[] loganScores = [90, 95, 87, 88, 96];
 
-int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
+string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
 
-foreach (int score in sophiaScores)
-{
-    sophiaSum += score;
-}
-
-foreach (int score in andrewScores)
-{
-    andrewSum += score;
-}
-foreach (int score in emmaScores)
-{
-    emmaSum += score;
-}
-foreach (int score in loganScores)
-{
-    loganSum += score;
-}
-
-decimal sophiaScore = (decimal)sophiaSum / currentAssignments;
-decimal andrewScore = (decimal)andrewSum / currentAssignments;
-decimal emmaScore = (decimal)emmaSum / currentAssignments;
-decimal loganScore = (decimal)loganSum / currentAssignments;
+int[] studentScores = new int[10];
 
 Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    int sumAssignmentScores = 0;
+
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        sumAssignmentScores += score;
+    }
+    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+
+    Console.WriteLine($"{currentStudent}:\t\t{currentStudentGrade}\t?");
+}
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
--- GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a220b8c3cb6a644c7ea56a8eb01bf2e62ec403c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c5b50ef452508f7b0852b9b9b18212d17516ed7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
